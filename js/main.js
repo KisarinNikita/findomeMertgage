e3e88f8bfdb01ab94bb9857bdf677f6ea5b3a697
Kisarin Nikita <kisarinnnikita@gmail.com>
2017-05-24 05:15:49 +0700

    !

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-
-
-
-$(document).ready(function() {
-
-  $("#app_cost").ionRangeSlider({
-      min: 180,
-      max: 30000,
-      from: 500,
-      step: 50,
-      postfix: "тыч.руб"
-  });
-
-  $("#paste").ionRangeSlider({
-      type: "double",
-      min: 6,
-      max: 1000,
-      from: 200,
-      to: 800
-  });
-
-  $("#period").ionRangeSlider({
-      type: "double",
-      min: 1,
-      max: 30,
-      from: 3,
-      to: 10
-  });
-
-});
-=======
 const basePaste = 0.114;
 const quareDiscount = 0.01;
 const vtb = 0.005;
@@ -68,10 +36,9 @@ let countOfCredit = function(priceFlat, firstSum) {
 }
 
 let MonthlyPayment = function(summCredit, creditTerm, currentPaste) {
-	return pow(((summCredit * kVTB * currentPaste) / (1 - (1 + kVTB * currentPaste)), -creditTerm);
+	return pow(((summCredit * kVTB * currentPaste) / (1 - (1 + kVTB * currentPaste)), -creditTerm));
 }
 
 let mustMonthlyIncome = function(monthlyPayment) {
 	return monthlyPayment * k1VTB;
 }
->>>>>>> Stashed changes
