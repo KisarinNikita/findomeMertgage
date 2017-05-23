1826010d3e9a00c419e63a981425934422db3c1b
Viktor <vchernetsof@gmail.com>
2017-05-23 21:03:05 +0700

    Формулы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 
@@ -28,3 +29,49 @@ $(document).ready(function() {
   });
 
 });
+=======
+const basePaste = 0.114;
+const quareDiscount = 0.01;
+const vtb = 0.005;
+const onlyTwoDocs = 0.005;
+const kVTB = 1 / 12;
+const k1VTB = 1.6;
+
+$(document).ready(function() {
+	$("#app_cost").ionRangeSlider({
+	      min: 180,
+	      max: 30000,
+	      from: 500,
+	      step: 50,
+	      postfix: "тыч.руб"
+ 	 });
+
+  $("#paste").ionRangeSlider({
+	      type: "double",
+	      min: 6,
+	      max: 1000,
+	      from: 200,
+	      to: 800
+  	});
+
+  $("#period").ionRangeSlider({
+	      type: "double",
+	      min: 1,
+	      max: 30,
+	      from: 3,
+	      to: 10
+  	});
+});
+
+let countOfCredit = function(priceFlat, firstSum) {
+	return priceFlat - firstSum;
+}
+
+let MonthlyPayment = function(summCredit, creditTerm, currentPaste) {
+	return pow(((summCredit * kVTB * currentPaste) / (1 - (1 + kVTB * currentPaste)), -creditTerm);
+}
+
+let mustMonthlyIncome = function(monthlyPayment) {
+	return monthlyPayment * k1VTB;
+}
+>>>>>>> Stashed changes
